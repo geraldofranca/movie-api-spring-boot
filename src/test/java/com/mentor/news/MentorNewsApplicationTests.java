852b37e522b2dec4fa1ed022531568110d5b8be9
Geraldo Franca <geraldo.franca@mentorti.com.br>
2021-01-08 17:41:50 -0300

    chore: created project

---
+++ src/test/java/com/mentor/news/MentorNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentor.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
